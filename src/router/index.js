175166aa371820bcb15d7ffd889d9d106bb9441f
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-03-13 01:52:16 +0530

    props for id

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Meetups from '@/components/Meetup/Meetups'
 import CreateMeetup from '@/components/Meetup/CreateMeetup'
+import Meetup from '@/components/Meetup/Meetup'
 import Profile from '@/components/User/Profile'
 import Register from '@/components/User/Register'
 import Login from '@/components/User/Login'
@@ -36,6 +37,12 @@ export default new Router({
       path: '/meetups/create',
       name: 'CreateMeetup',
       component: CreateMeetup
+    },
+    {
+      path: '/meetups/:id',
+      name: 'Meetup',
+      props: true,
+      component: Meetup
     },
     {
       path: '/profile',
