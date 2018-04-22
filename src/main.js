fc925180bfb21cb9114d8f16a4c5bc48b9da8a29
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-04-23 01:51:01 +0530

    firebase configuration setup

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import {store} from './store'
-
+import DateFilter from './filters/date'
+import AlertCmp from './components/Shared/Alert.vue';
 Vue.use(Vuetify)
 Vue.config.productionTip = false
+Vue.filter('date',DateFilter)
+Vue.component('app-alert',AlertCmp);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAOLUqy2iT3TMHldAj5oKUP0Oa-vdqOaas",
+      authDomain: "vuefs-prod-e708a.firebaseapp.com",
+      databaseURL: "https://vuefs-prod-e708a.firebaseio.com",
+      projectId: "vuefs-prod-e708a",
+      storageBucket: "vuefs-prod-e708a.appspot.com"
+    });
+  }
 })
