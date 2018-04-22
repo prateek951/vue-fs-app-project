0022f9ed39f26d93d09b07d27c7edecd1bf9491d
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-04-23 01:54:18 +0530

    alert added but still doesn't work

---
+++ src/components/Shared/Alert.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-alert error dismissible @input="onClose" :value="true">
+      {{text}}
+  </v-alert>
+</template>
+
+
+<script>
+export default {
+    props: ['text'],
+    /*Emit an event on the close of the alert*/
+    onClose(){
+        this.$emit('dismissed');
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
